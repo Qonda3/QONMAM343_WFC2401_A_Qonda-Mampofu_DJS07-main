396609ae3f40d6db6299625836e6cc9bbf656af6
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-06-03 13:30:17 +0200

    Add semantic comments on Header file

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,14 @@
-
 function Header() {
   return (
     <header className="header">
+      {/* Display the troll face image */}
       <img 
         src="./images/troll-face.png" 
         className="header--image"
       />
+      {/* Display the title */}
       <h2 className="header--title">Meme Generator</h2>
+      {/* Display the project information */}
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
